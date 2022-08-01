39dfa2385f9e49516f6c151cf55acb5da93492b7
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-07-31 22:34:43 -0700

    state change

--- src/main/java/frc/robot/constants/StaticStates.java
+++ src/main/java/frc/robot/constants/StaticStates.java
@@ -1,21 +1,6 @@
 package frc.robot.constants;
 
 public class StaticStates {
-    public static enum SolenoidState {
-        EXTEND(true),
-        RETRACT(false);
-
-        private final boolean state;
-
-        private SolenoidState(boolean state) {
-            this.state = state;
-        }
-
-        public boolean get() {
-            return state;
-        }
-    }
-
     public static enum ShooterState {
         TARMAC,
         FENDER,
